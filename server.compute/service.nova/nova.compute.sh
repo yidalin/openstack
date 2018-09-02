f2d3bb97fa45f9a6c7b37125f4d232247b9d9ef4
Davis Lin <da860610@gmail.com>
2018-09-02 16:23:21 +0800

    Modify the nova.compute.sh

--- server.compute/service.nova/nova.compute.sh
+++ server.compute/service.nova/nova.compute.sh
@@ -1,2 +1,9 @@
 # Implement on the compute server
 apt install -y nova-compute
+
+cp -af nova.conf.no-comment /etc/nova/nova.conf
+cp -af nova-compute.conf.no-comment /etc/nova/nova-compute.conf
+
+systemctl restart nova-compute.service
+
+systemctl status nova-compute.service
