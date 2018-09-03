181f19054feee343226cb4aefb280b5d9bbef649
Davis Lin <da860610@gmail.com>
2018-09-03 23:04:17 +0800

    Modify nova and neutron conf

--- server.controller/service.neutron/neutron.controller.sh
+++ server.controller/service.neutron/neutron.controller.sh
@@ -3,5 +3,5 @@ apt install -y neutron-server neutron-plugin-ml2 python-neutronclient
 
 cp -af neutron.conf.no-comment /etc/neutron/neutron.conf 
 cp -af ml2_conf.ini.orig.no-comment /etc/neutron/plugins/ml2/ml2_conf.ini
-
+cp -af nova.conf.no-comment /etc/nova/nova.conf
 break
