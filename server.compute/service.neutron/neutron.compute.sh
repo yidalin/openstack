c52457e17880692bd816a35ad71143ced03fb7d3
Davis <da860610@gmail.com>
2018-09-04 00:56:03 +0800

    Add some conf file for neutron

--- server.compute/service.neutron/neutron.compute.sh
+++ server.compute/service.neutron/neutron.compute.sh
@@ -1,2 +1,10 @@
 # Install neutron-linuxbridge-agent package
 apt install -y neutron-linuxbridge-agent
+
+cp -af neutron.conf.no-comment /etc/neutron/neutron.conf
+cp -af linuxbridge_agent.ini.no-comment /etc/neutron/plugins/ml2/linuxbridge_agent.ini
+cp -af nova.conf.no-comment /etc/nova/nova.conf
+
+systemctl restart nova-compute.service
+systemctl restart neutron-linuxbridge-agent.service
+
