47150c3e5d9877aad1265b1037d06eb30078b7b8
Davis Lin <da860610@gmail.com>
2018-09-03 23:10:02 +0800

    Modify neutron.controller.sh

--- server.controller/service.neutron/neutron.controller.sh
+++ server.controller/service.neutron/neutron.controller.sh
@@ -4,4 +4,8 @@ apt install -y neutron-server neutron-plugin-ml2 python-neutronclient
 cp -af neutron.conf.no-comment /etc/neutron/neutron.conf 
 cp -af ml2_conf.ini.orig.no-comment /etc/neutron/plugins/ml2/ml2_conf.ini
 cp -af nova.conf.no-comment /etc/nova/nova.conf
-break
+
+neutron-db-manage --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugins/ml2/ml2_conf.ini upgrade head
+
+systemctl restart nova-api.service
+systemctl restart neutron-server.service
