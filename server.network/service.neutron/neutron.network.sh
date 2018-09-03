a64241e57f667839616a72f339d00f44b22e2f67
Davis Lin <da860610@gmail.com>
2018-09-03 23:40:35 +0800

    Modify neutron.network.sh

--- server.network/service.neutron/neutron.network.sh
+++ server.network/service.neutron/neutron.network.sh
@@ -1,1 +1,7 @@
-test
+## Implement on neutron server
+
+# Install neutron layer 2 and layer 3 component
+apt install -y neutron-plugin-ml2 neutron-linuxbridge-agent \
+neutron-l3-agent neutron-dhcp-agent neutron-metadata-agent
+
+cp -af neutron.conf.no-comment /etc/neutron/neutron.conf
