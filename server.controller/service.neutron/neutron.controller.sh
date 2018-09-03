f36e5ca7361da0b43eb5e33f62ceca82d75b2f19
Davis Lin <da860610@gmail.com>
2018-09-03 22:32:15 +0800

    Modify neutron.controller.sh

--- server.controller/service.neutron/neutron.controller.sh
+++ server.controller/service.neutron/neutron.controller.sh
@@ -1,4 +1,7 @@
 # Install neutron service on Controller
 apt install -y neutron-server neutron-plugin-ml2 python-neutronclient
 
-/etc/neutron/neutron.conf
+cp -af neutron.conf.no-comment /etc/neutron/neutron.conf 
+cp -af ml2_conf.ini.orig.no-comment /etc/neutron/plugins/ml2/ml2_conf.ini
+
+break
