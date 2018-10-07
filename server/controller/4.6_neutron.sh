57c20790918bf4189c28225c0245bafb854beb57
Davis Lin <da860610@gmail.com>
2018-10-08 04:11:01 +0800

    fix

--- server/controller/4.6_neutron.sh
+++ server/controller/4.6_neutron.sh
@@ -8,13 +8,13 @@ source ~/admin-openrc
 # Cteate ext-net external network (flat)
 openstack network create ext-net --external --provider-physical-network external --provider-network-type flat
 # Create a subnet for ext-net
-openstack subnet create ext-subnet --network ext-net --subnet-range 192.168.9.0/24 --allocation-pool start=192.168.9.220,end=192.168.9.240 --no-dhcp --gateway 192.168.9.254
+openstack subnet create ext-subnet --network ext-net --subnet-range 192.168.0.0/24 --allocation-pool start=192.168.9.220,end=192.168.9.240 --no-dhcp --gateway 192.168.0.1
 
 source ~/demo-openrc
 
 # Use demo account
 openstack network create demo-net
-openstack subnet create demo-subnet --network demo-net --subnet-range 192.168.0.0/24 --dns-nameserver 8.8.8.8 --gateway 192.168.0.1
+openstack subnet create demo-subnet --network demo-net --subnet-range 192.168.100.0/24 --dns-nameserver 8.8.8.8 --gateway 192.168.100.1
 openstack router create demo-router
 openstack router add subnet demo-router demo-subnet
 openstack router set demo-router --external-gateway ext-net
